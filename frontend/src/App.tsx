33dfbf256283940faa4e1887915e153ee8eabc73
Aditi1900 <aditi.v.dharmadhikari@gmail.com>
2024-11-08 00:23:57 -0500

    Slight changes (mostly ignore this commit)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './shared/Navbar';
-//import Homepage from './pages/Homepage';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-import HeroSection from "../src/pages/HeroSection";
+import Homepage from './pages/Homepage';
 const App: React.FC = () => {
   return (
     <Router>
@@ -13,7 +12,7 @@ const App: React.FC = () => {
       
       <div className="mt-16">
         <Routes>
-          <Route path="/" element={<HeroSection />} />
+          <Route path="/" element={<Homepage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
